57834bd982ed99ade463d45edcccbf463beca03a
Jimmy Yao <42041438+6PackOfRibs@users.noreply.github.com>
2023-10-07 21:48:09 -0400

    setup frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from './student';
+import { StudentService } from './student.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'studentclassapp';
+export class AppComponent implements OnInit {
+  public students!: Student[];
+
+  constructor(private studentService: StudentService){}
+
+  ngOnInit() {
+      this.getStudents();
+  }
+
+  public getStudents(): void {
+    this.studentService.getStudents().subscribe(
+      (response: Student[]) => {
+        this.students = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
 }
