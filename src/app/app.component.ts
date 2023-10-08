50f38ad28b23dee05936400dfab96034428f57aa
Jimmy Yao <42041438+6PackOfRibs@users.noreply.github.com>
2023-10-07 23:42:32 -0400

    Allow buttons to pull up forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,4 +27,23 @@ export class AppComponent implements OnInit {
       }
     )
   }
+
+  public onOpenModal(student: Student | null, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addStudentModal');
+    }
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#editStudentModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteStudentModal');
+    }
+    container!.appendChild(button);
+    button.click();
+  }
 }
